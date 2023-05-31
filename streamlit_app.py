4f2ba219baee58f1e9a1755767e4253b71e055c3
THAMARAISELVAN1234 <135097362+THAMARAISELVAN1234@users.noreply.github.com>
2023-05-31 13:10:31 +0530

    Update streamlit_app.py
    importing fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@ import streamlit
 streamlit.title('Hello Everyone')
 streamlit.header('My profile')
 streamlit.text('Myself Thamaraiselvan')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
